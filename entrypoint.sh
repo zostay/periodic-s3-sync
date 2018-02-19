10b42d94c0f18c1bb279791bf8e5af5fba8d3d6d
Sterling Hanenkamp <sterling@hanenkamp.com>
2018-02-19 12:17:38 -0600

    Oops.

--- entrypoint.sh
+++ entrypoint.sh
@@ -34,22 +34,6 @@ for var in $AWS_ENV; do
     fi
 done
 
-if [[ "$ROLE_ARN" -a -z "$AWS_ACCESS_KEY_ID" ]]; then
-    echo "Operating as $ROLE_ARN"
-
-    session_info=$(aws sts assume-role \
-        --role-arn $ROLE_ARN \
-        --role-session-name periodic-s3-sync)
-
-    if [[ "$DEBUG_SESSION_INFO" ]]; then
-        echo $session_info
-    fi
-
-    export AWS_ACCESS_KEY_ID=$(echo $session_info | jq -r .Credentials.AccessKeyId)
-    export AWS_SECRET_ACCESS_KEY=$(echo $session_info | jq -r .Credentials.SecretAccessKey)
-    export AWS_SESSION_TOKEN=$(echo $session_info | jq -r .Credentials.SessionToken)
-fi
-
 if [[ "$STARTUP_SYNC" == 1 ]]; then
     /s3-sync.sh
 fi
