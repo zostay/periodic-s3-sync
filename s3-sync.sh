6f1edf5f8696fc1c2ac6ea8de2dc6e636985fc78
Sterling Hanenkamp <sterling@hanenkamp.com>
2021-12-17 13:24:40 -0600

    trying to make the file completion better

--- s3-sync.sh
+++ s3-sync.sh
@@ -8,14 +8,16 @@ aws s3 sync $SYNC_FROM $SYNC_TO $SYNC_PARAMS
 
 # touch the completion file somewhere locally if requested on completion, but
 # only do this during the startup sync.
-if [[ -n "$__STARTUP_SYNC_IN_PROGRESS" && -n "$COMPLETION_FILENAME" ]]; then
+if [[ "$__STARTUP_SYNC_IN_PROGRESS" && "$COMPLETION_FILENAME" ]]; then
     if [[ "$SYNC_TO" != "s3:"* ]]; then
-        echo "TOUCH $SYNC_TO/$COMPLETION_FILE"
         touch "$SYNC_TO/$COMPLETION_FILE"
+        chmod a+r "$SYNC_TO/$COMPLETION_FILE"
+        echo $(date) TOUCH $SYNC_TO/$COMPLETION_FILE
     fi
     if [[ "$SYNC_FROM" != "s3:"* ]]; then
-        echo "TOUCH $SYNC_FROM/$COMPLETION_FILE"
         touch "$SYNC_FROM/$COMPLETION_FILE"
+        chmod a+r "$SYNC_FROM/$COMPLETION_FILE"
+        echo $(date) TOUCH $SYNC_FROM/$COMPLETION_FILE
     fi
 fi
 
