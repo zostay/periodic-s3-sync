b6be05bcd30e73e68408ad64c1ffe0573e1be64a
Sterling Hanenkamp <sterling@hanenkamp.com>
2021-12-17 13:36:53 -0600

    fixing silly misspelling of COMPLETION_FILENAME

--- s3-sync.sh
+++ s3-sync.sh
@@ -10,14 +10,14 @@ aws s3 sync $SYNC_FROM $SYNC_TO $SYNC_PARAMS
 # only do this during the startup sync.
 if [[ "$__STARTUP_SYNC_IN_PROGRESS" && "$COMPLETION_FILENAME" ]]; then
     if [[ "$SYNC_TO" != "s3:"* ]]; then
-        touch "$SYNC_TO/$COMPLETION_FILE"
-        chmod a+r "$SYNC_TO/$COMPLETION_FILE"
-        echo $(date) TOUCH $SYNC_TO/$COMPLETION_FILE
+        touch "$SYNC_TO/$COMPLETION_FILENAME"
+        chmod a+r "$SYNC_TO/$COMPLETION_FILENAME"
+        echo $(date) TOUCH $SYNC_TO/$COMPLETION_FILENAME
     fi
     if [[ "$SYNC_FROM" != "s3:"* ]]; then
-        touch "$SYNC_FROM/$COMPLETION_FILE"
-        chmod a+r "$SYNC_FROM/$COMPLETION_FILE"
-        echo $(date) TOUCH $SYNC_FROM/$COMPLETION_FILE
+        touch "$SYNC_FROM/$COMPLETION_FILENAME"
+        chmod a+r "$SYNC_FROM/$COMPLETION_FILENAME"
+        echo $(date) TOUCH $SYNC_FROM/$COMPLETION_FILENAME
     fi
 fi
 
